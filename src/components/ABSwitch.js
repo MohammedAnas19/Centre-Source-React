f42fbaa51622b77a15f15029f4dd0feea48d6f7c
MohammedAnas19 <mohammedanas44@gmail.com>
2021-08-25 21:08:46 +0530

    Seventh Commit

--- src/components/ABSwitch.js
+++ src/components/ABSwitch.js
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
 
 function ABSwitch({ availableOpt }) {
-  const [active, setActive] = useState(true);
-  const toggleController = (opt) => {
-    setActive(!active);
-  };
+  // const [active, setActive] = useState(true);
+  // const toggleController = (opt) => {
+  //   setActive(!active);
+  // };
   return (
     <div className="ab-switch">
       <span
-        onClick={() => toggleController()}
+        // onClick={() => toggleController()}
         className={availableOpt ? "ab-active" : " "}
       >
         A
       </span>
       <span
-        onClick={() => toggleController()}
+        // onClick={() => toggleController()}
         className={availableOpt ? " " : " ab-active"}
       >
         B
